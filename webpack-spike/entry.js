ee7f995ca85bdad5b9b11f71ef1d74335774e1c1
Pete Hodgson <git@thepete.net>
2016-10-07 06:14:16 -0700

    use require context to load all tests automagically

--- webpack-spike/entry.js
+++ webpack-spike/entry.js
@@ -1,5 +1,8 @@
 const chai = require('chai');
 global.expect = chai.expect;
 
-require('./test/addition.test.js');
-require('./test/more-addition.test.js');
+const testsContext = require.context('./test', true, /\.test\.js$/);
+
+testsContext.keys().forEach(function (key) {
+  const testModule = testsContext(key);
+});
